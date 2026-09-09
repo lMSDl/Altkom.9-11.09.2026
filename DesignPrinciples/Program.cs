e13f927fdee9af11e9bf25ebf2f6b5a55d926043
Paweł <pawel@msd.info.pl>
2026-09-09 10:49:34 +0200

    DesignPrinciples

---
+++ DesignPrinciples/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using DesignPrinciples;
+
+var service = new Class2();
+
+var custmerId = 1;
+
+service.Func2(custmerId, 500);
+if (service.Func1(custmerId, 100))
+{
+    Console.WriteLine($"Customer {custmerId} charged. Actual balace: {service.GetBalance(custmerId)}");
+}
+else
+{
+    Console.WriteLine($"Customer {custmerId} cannot be charged. Actual balace: {service.GetBalance(custmerId)}");
+}
