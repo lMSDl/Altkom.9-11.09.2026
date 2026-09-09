ec377281153e1ac9bb7e09c6be7bd5b5df383c9e
Paweł <pawel@msd.info.pl>
2026-09-09 13:55:37 +0200

    ISP

---
+++ SOLID/ISP.cs
@@ -0,0 +1,36 @@
+﻿namespace SOLID.I
+{
+    interface IExcelExporter
+    {
+        void ToExcel();
+    }
+
+    interface IPdfExporter
+    {
+        void ToPdf();
+    }
+
+    class Report : IExcelExporter, IPdfExporter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Exporting report to PDF...");
+        }
+        public void ToExcel()
+        {
+            Console.WriteLine("Exporting report to Excel...");
+        }
+    }
+
+    class Poem : IPdfExporter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Exporting poem to PDF...");
+        }
+        /*public void ToExcel()
+        {
+            throw new NotImplementedException("Poem cannot be exported to Excel.");
+        }*/
+    }
+}
