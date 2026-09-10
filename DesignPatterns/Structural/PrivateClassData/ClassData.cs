f572c8313a1d380155b00381400b2a0e5e8760b6
Paweł <pawel@msd.info.pl>
2026-09-10 15:10:12 +0200

    PrivateClassData

---
+++ DesignPatterns/Structural/PrivateClassData/ClassData.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Structural.PrivateClassData
+{
+    internal class ClassData
+    {
+        //private readonly string _someString;
+        //public int SomeInt { get; }
+        private PrivateData _privateData;
+
+        public ClassData(string someString, int someInt)
+        {
+            //_someString = someString;
+            //SomeInt = someInt;
+            _privateData = new PrivateData(someString, someInt);
+        }
+
+
+        public void DoSth()
+        {
+            //_someString = "";
+            //SomeInt = 0;
+
+            var someString = _privateData.GetSomeString();
+            var someInt = _privateData.GetSomeInt();
+        }
+    }
+}
---
+++ DesignPatterns/Structural/PrivateClassData/PrivateData.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatterns.Structural.PrivateClassData
+{
+    internal class PrivateData
+    {
+        private string _someString;
+        private int _someInt;
+
+        public PrivateData(string someString, int someInt)
+        {
+            _someString = someString;
+            _someInt = someInt;
+        }
+
+        public string GetSomeString()
+        {
+            return _someString;
+        }
+
+        public int GetSomeInt()
+        {
+            return _someInt;
+        }
+    }
+}
